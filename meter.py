d98fc69912b833eb2579d0861393ba93315e2402
Tony Gonelli <gonelli@utexas.edu>
2020-03-17 01:17:58 -0500

    General curve generated

--- meter.py
+++ meter.py
@@ -4,8 +4,8 @@ import json
 import random
 import datetime
 
-currentDate = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-currentTime = currentDate
+startTime = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+currentTime = startTime
 
 def publish(message):
     pvChannel = connection.channel()
@@ -15,19 +15,29 @@ def publish(message):
 
 def nextMessage():
     global currentTime
-    # print(currentTime.strftime("%Y-%m-%d %H:%M:%S"))
-
     message = {
         "time": currentTime.strftime("%Y-%m-%d %H:%M:%S"),
-        "pv": random.randrange(0, 15, 1)
+        "pv": getPVFromTime()
     }
-
-    currentTime += datetime.timedelta(minutes=1)
-
+    currentTime += datetime.timedelta(minutes=5)
     return message
 
+def getPVFromTime():
+    if currentTime.hour < 6 or currentTime.hour >= 21:
+        return 0
+
+    x = (currentTime - startTime).total_seconds() / 60 / 60
+
+    if currentTime.hour >= 6 and currentTime.hour < 8:
+        return 125 * x - 750
+    if currentTime.hour >= 20 and currentTime.hour < 21:
+        return -150 * x + 3150
+
+    return (-700 * x**2 / 9) + (19525 * x / 9) - (109150 / 9)
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))    
-while currentTime < currentDate + datetime.timedelta(days=1): #currentTime + inverval < ...
+# currentTime + inverval < ...
+while currentTime < startTime + datetime.timedelta(days=1):
     publish(nextMessage())
 publish(None)
 connection.close()
